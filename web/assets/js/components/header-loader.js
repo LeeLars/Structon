32b8ce527d78f7b353ecfae0e9804df384e4c6ff
Lars Leenders <larsleenders@Larss-MacBook-Pro.local>
2026-02-10 10:33:24 +0100

    Fix mobile menu: restore categories, hide brands section, add 'Bekijk alles' button

--- web/assets/js/components/header-loader.js
+++ web/assets/js/components/header-loader.js
@@ -467,6 +467,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><polyline points="9 11 12 14 22 4"></polyline><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path></svg>
         Adapters
       </a>
+      <a href="${basePath}producten/" class="nav-link-more">Bekijk alles →</a>
     </div>
 
     <!-- Browse by Brand -->
